1a73381ba2fef0e239171d0454b48f4465257ea5
Sergey Omelchuk <sergomel52@gmail.com>
2020-03-30 22:25:05 +0200

    Added stuff to proxy my WCF WS properly(Manager and Proxies)

---
+++ XamarinExchangeApp/XamarinExchangeApp/Services/WCFExchangeServiceProxy/Constants.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinExchangeApp.Services.WCFExchangeServiceProxy
+{
+    public static class Constants
+    {
+        public static string SoapUrl
+        {
+            get
+            {
+                var defaultUrl = "http://localhost:55987/ExchangeService.svc";
+
+                if (Device.RuntimePlatform == Device.Android)
+                {
+                    defaultUrl = "http://10.0.2.2:55987/ExchangeService.svc";
+                }
+                else if (Device.RuntimePlatform == Device.iOS)
+                {
+                    defaultUrl = "http://192.168.1.143:55987/ExchangeService.svc";
+                }
+
+                return defaultUrl;
+            }
+        }
+    }
+}
---
+++ XamarinExchangeApp/XamarinExchangeApp/Services/WCFExchangeServiceProxy/ExchangeRatesServiceProxy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFExchangeService;
+using XamarinExchangeApp.Models;
+
+namespace XamarinExchangeApp.Services.WCFExchangeServiceProxy
+{
+    class ExchangeRatesServiceProxy : IExchangeRatesServiceProxy
+    {
+        IExchangeService exchangeService;
+
+        public List<Models.ExchangeRateType> Items { get; private set; }
+
+        public ExchangeRatesServiceProxy()
+        {
+            exchangeService = new ExchangeServiceClient(
+                new BasicHttpBinding(),
+                new EndpointAddress(Constants.SoapUrl));
+        }
+
+		WCFExchangeService.ExchangeRateType ToWCFServiceExchangeRateItem(Models.ExchangeRateType item)
+		{
+			return new WCFExchangeService.ExchangeRateType
+			{
+				Code = item.Code,
+				Currency = item.Currency,
+				Date = item.Date,
+				Rate = item.Rate
+			};
+		}
+
+		static Models.ExchangeRateType FromWCFServiceExchangeRateItem(WCFExchangeService.ExchangeRateType item)
+		{
+			return new Models.ExchangeRateType
+			{
+				Code = item.Code,
+				Currency = item.Currency,
+				Date = item.Date,
+				Rate = item.Rate
+			};
+		}
+
+		public Task<List<Models.ExchangeRateType>> RefreshDataAsync()
+        {
+			Items = new List<Models.ExchangeRateType>();
+
+			try
+			{
+				var ExchangeRateItems =  exchangeService.GetExchangeRateTypes();
+				foreach (var item in ExchangeRateItems)
+				{
+					Items.Add(FromWCFServiceExchangeRateItem(item));
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(@"				ERROR {0}", ex.Message);
+			}
+
+			return Task.FromResult(Items);
+		}
+    }
+}
